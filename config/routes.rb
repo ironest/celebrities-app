9eb6c44287e82535f1201aebde4a9009ca065388
ironest <ironest@gmail.com>
2019-10-03 21:57:03 +1000

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get "/", to: "celebrities#index", as: "root"
+  get "/celebrities/:id", to: "celebrities#show", as: "celebrity"
+  post "/celebrities/", to: "celebrities#create"
+  put "/celebrities/:id", to: "celebrities#update"
+  patch "/celebrities/:id", to: "celebrities#update"
+  delete "/celebrities/:id", to: "celebrities#destroy"
+
 end
